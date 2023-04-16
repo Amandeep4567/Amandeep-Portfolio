95c0aa53bb290a6a7abf618560a85b8bb903cc8d
Amandeep Kalakar <amankumaradk@gmail.com>
2023-04-16 21:13:47 +0530

    feat: code updated

---
+++ src/components/EducationAndCertification.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const EducationAndCertification = () => {
+  return <div>EducationAndCertification</div>;
+};
+
+export default EducationAndCertification;
---
+++ src/components/index.js
@@ -0,0 +1,19 @@
+import ContactInformation from "./ContactInformation";
+import EducationAndCertification from "./EducationAndCertification";
+import HomePage from "./HomePage";
+import Navbar from "./Navbar";
+import Preloader from "./Preloader";
+import ProjectSection from "./ProjectSection";
+import TechnicalSkills from "./TechnicalSkills";
+import WorkExperience from "./WorkExperience";
+
+export {
+  ContactInformation,
+  EducationAndCertification,
+  HomePage,
+  Navbar,
+  Preloader,
+  ProjectSection,
+  TechnicalSkills,
+  WorkExperience,
+};
