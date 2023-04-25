432ce7d7a8980ead83b3fd3f0eb82875576693aa
Amandeep Kalakar <amankumaradk@gmail.com>
2023-04-25 18:35:02 +0530

    feat: code updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,41 @@
 import React from "react";
+import {
+  ContactInformation,
+  EducationAndCertification,
+  HomePage,
+  Navbar,
+  Preloader,
+  ProjectSection,
+  TechnicalSkills,
+  WorkExperience,
+} from "./components";
 
-const App = () => {
-  return <div>App</div>;
-};
+const App = () => (
+  <div className="w-full overflow-hidden">
+    <div className="home w-full h-screen">
+      <div className="">
+        <div className="">
+          <Navbar />
+        </div>
+      </div>
+
+      <div className="">
+        <div className="">
+          <HomePage />
+        </div>
+      </div>
+    </div>
+
+    <div className="">
+      <div className="">
+        <ProjectSection />
+        <TechnicalSkills />
+        <EducationAndCertification />
+        <WorkExperience />
+        <ContactInformation />
+      </div>
+    </div>
+  </div>
+);
 
 export default App;
