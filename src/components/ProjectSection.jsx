3e138eded26a95db0b95cfa4a13f82874ec4140c
Amandeep Kalakar <amankumaradk@gmail.com>
2023-05-02 22:02:22 +0530

    feat: code updated

--- src/components/ProjectSection.jsx
+++ src/components/ProjectSection.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 const ProjectSection = () => {
   return (
@@ -6,8 +8,30 @@ const ProjectSection = () => {
       <div className="flex ml-[100px]">
         <h1 className="font-semibold text-[36px]">My Projects</h1>
       </div>
-      <div className="">
-        <div className=""></div>
+      <div className="mt-14">
+        <div className="flex justify-center">
+          <div className="">
+            <h2>Pokemon Card Game</h2>
+            <p>Collect and battle with cute pocket monsters on cards.</p>
+            <div className="">
+              <FontAwesomeIcon icon={faGithub} className="iconTech m-2" />
+            </div>
+          </div>
+          <div className="">
+            <h2 className="">Pokemon Card Game</h2>
+            <p>Collect and battle with cute pocket monsters on cards.</p>
+            <div className="">
+              <FontAwesomeIcon icon={faGithub} className="iconTech m-2" />
+            </div>
+          </div>
+          <div className="">
+            <h2>Pokemon Card Game</h2>
+            <p>Collect and battle with cute pocket monsters on cards.</p>
+            <div className="">
+              <FontAwesomeIcon icon={faGithub} className="iconTech m-2" />
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
